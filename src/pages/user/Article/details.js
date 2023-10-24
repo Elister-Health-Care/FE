bd198f53d2e1669b66c0402f9a84357aa0c4a468
tuandat25112002 <dtdat.20it1@vku.udn.vn>
2023-10-24 08:41:47 +0700

    fix navbar

---
+++ src/pages/user/Article/details.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import "./article.css";
+import { Container } from "reactstrap";
+import LoadingDot from '~/components/Loading/LoadingDot'
+
+const ArticleDetail = () => {
+
+}
+
+export default ArticleDetail
